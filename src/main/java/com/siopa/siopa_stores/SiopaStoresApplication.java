00d22b8706b13392f6acd8ae24717a1100392f42
Ross Gartland <rgartland117@hotmail.com>
2025-02-23 18:32:34 +0000

    first commit

---
+++ src/main/java/com/siopa/siopa_stores/SiopaStoresApplication.java
@@ -0,0 +1,13 @@
+package com.siopa.siopa_stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiopaStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiopaStoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siopa/siopa_stores/SiopaStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopa.siopa_stores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiopaStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
