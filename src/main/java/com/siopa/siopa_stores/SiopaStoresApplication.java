6a1a0d83ec6c88b8f3ca397bf24b1609c0acca17
Ross Gartland <rgartland117@hotmail.com>
2025-03-03 21:42:01 +0000

    Connected service to eureka

--- src/main/java/com/siopa/siopa_stores/SiopaStoresApplication.java
+++ src/main/java/com/siopa/siopa_stores/SiopaStoresApplication.java
@@ -2,8 +2,10 @@ package com.siopa.siopa_stores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SiopaStoresApplication {
 
 	public static void main(String[] args) {
